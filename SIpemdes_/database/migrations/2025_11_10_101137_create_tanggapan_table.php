7c2ff63e66e142e6db562a4e3a34a1d8d5742351
kartikaaadn <2317051070@students.unila.ac.id>
2025-11-10 17:14:32 +0700

    buat database

--- SIpemdes_/database/migrations/2025_11_10_101137_create_tanggapan_table.php
+++ SIpemdes_/database/migrations/2025_11_10_101137_create_tanggapan_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('masyarakat', function (Blueprint $table) {
+            $table->id('id_masyarakat');
+            $table->string('nik', 16)->unique();
+            $table->string('nama', 100);
+            $table->text('alamat');
+            $table->string('no_hp', 15);
+            $table->string('username', 50)->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('masyarakat');
     }
 };
